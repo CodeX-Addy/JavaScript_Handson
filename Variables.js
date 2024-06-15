69555843e077e7ace5701a08dfbd1cf305d98565
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-06-15 21:09:46 +0530

    .

---
+++ Variables.js
@@ -0,0 +1,8 @@
+// Pointer 1: Dynamically typed
+let a = 8;
+console.log(a); // 8
+a = "Eso";
+console.log(a); // Eso
+
+// Pointer 2: var is globally, while let is locally scoped
+//Nowadays only let is widely used 
