ae39de9abc59da532cd893ea11c9833e7f1e0a34
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-06-15 21:44:40 +0530

    .

--- Primitive-Objects.js
+++ Primitive-Objects.js
@@ -6,3 +6,13 @@ S : Symbol
 B : Boolean
 B : BigInt
 U : undefined */
+
+let a = null;
+let b = 527;
+let c = "Eso";
+let d = Symbol("A symbol type");
+let e = true;
+let f = BigInt("528");
+let g;
+console.log(a,b,c,d,e,f,g);
+console.log(typeof a,typeof b,typeof c,typeof d,typeof e,typeof f, typeof g);
