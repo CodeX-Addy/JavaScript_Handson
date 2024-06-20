c38dab933fafc628b2c7b2aaa34769e44569e48d
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-06-20 17:40:18 +0530

    .

---
+++ Express-Cond.js
@@ -0,0 +1,11 @@
+let a = 45;
+let b = 4;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a / b); //It will give exact value in decimals unlike other languages that rounded off to 11
+console.log(a * b);
+console.log(a ** b);
+console.log(a % b);
+
+
