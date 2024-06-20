791b1772dc61bd7ff12315a24064d5577d54fcec
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-06-20 18:43:26 +0530

    js operators

--- Express-Cond.js
+++ Express-Cond.js
@@ -1,11 +1,30 @@
+//We'll cover here some new things in operators as most of the functionalities is more or less similar to other programming languages
+
+//Arithmetic operators
 let a = 45;
 let b = 4;
-
 console.log(a + b);
 console.log(a - b);
 console.log(a / b); //It will give exact value in decimals unlike other languages that rounded off to 11
 console.log(a * b);
 console.log(a ** b);
 console.log(a % b);
+
+//Comparison operators
+let c = "6";
+let d = 6;
+console.log(c == d); //true
+console.log(c != d); //false
+console.log(c === d); //false //it checks the comparison + type
+console.log(c !== d); //true
+
+//Logical operators
+let x = 5;
+let y = 10;
+
+console.log(x < y && x == 5);
+console.log(x < y || x == 5);
+console.log(!false);
+
 
 
