f3e72f12345177f1049871951196ab16770c9bba
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-06-21 11:06:31 +0530

    .

---
+++ Conditionals.js
@@ -0,0 +1,14 @@
+//let a = prompt("Enter your number: ");
+//console.log(typeof a) //String
+//a = Number.parseInt(a)
+//console.log(typeof a) //Number
+
+let b = 4;
+if(b>18)
+   console.log("You can Drive");
+else 
+    console.log("Go to home");
+
+//In the similar way, we can use else if statements
+
+
