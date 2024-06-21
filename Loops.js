df7141847f164a8d6bef0a03eb0e2748c51197b4
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-06-21 11:24:54 +0530

    .

--- Loops.js
+++ Loops.js
@@ -1,4 +1,4 @@
-//For loop with sum of n natural numbers example
+/*//For loop with sum of n natural numbers example
 let sum = 0;
 let n = prompt("Enter your number: ");
 n = Number.parseInt(n);
@@ -6,3 +6,17 @@ for(let i = 0; i < n; i++){
     sum += (i+1);
 }
 console.log(sum);
+*/
+
+//For In loops --> mostly used for objects
+
+let obj = {
+    student1 : 100,
+    student2 : 90,
+    student3 : 80,
+    student4 : 70
+}
+
+for(let i in obj){
+    console.log("Marks of " + i + " is " + obj[i]);
+}
