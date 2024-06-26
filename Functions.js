d114506a83d5eae60e3c93af4c42811a70461d06
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-06-26 19:39:40 +0530

    functions in js

--- Functions.js
+++ Functions.js
@@ -4,3 +4,10 @@ function avg(x, y){
 }
 
 console.log("Average of 2 and 3 is : ", avg(2,3));
+
+//Arrow function --> mostly used
+const average = (x, y) => {
+    return (x+y)/2;
+}
+
+console.log("Average of 4 and 5 is : ", average(4,5));
