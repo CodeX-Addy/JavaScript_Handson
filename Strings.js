e04dd9dc692f5022b3ab06231c0c48fb71c81626
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-06-26 20:09:55 +0530

    .

---
+++ Strings.js
@@ -0,0 +1,14 @@
+let a = "Esoteric";
+console.log(a.length);
+
+//Template literal
+
+let n1 = "John";
+let n2 = "Doe";
+
+console.log(`${n1} is a friend of ${n2}`)
+
+//Escape seq characters
+
+let f = "John \nDoe";
+console.log(f);
