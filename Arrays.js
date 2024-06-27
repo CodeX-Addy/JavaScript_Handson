2a762c4d9c9779a522b4d303ee5b49cbd3cfe778
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-06-27 19:14:01 +0530

    .

---
+++ Arrays.js
@@ -0,0 +1,13 @@
+//Arrays in js is like lists in python in which we can store the values with different datatypes unlike c++ and java and they are mutable unlike strings
+
+let arr = [1,2,3,4,"Eso"]
+//console.log(arr)
+
+//Arrays Methods in JS
+
+let a = [1,2,3,4];
+console.log(a.length); //length of the array
+let b = a.toString(); //Converts arrays into strings
+console.log(b);
+
+console.log(a.join("-")); //Join elements and returns a string
