d0971d1b0654f5bb518de34a8fb124d6e312704c
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-10-07 11:14:52 +0530

    scopes and lexical environment

--- Scopes.js
+++ Scopes.js
@@ -1,8 +1,14 @@
+//At very start, the global execution context is created
+
 // In JS, for each function invokation, an execution context will be created, that composed of memory and code section in function call stack
 
 //For each execution context, there is a space created for that referenced to the lexical environment of it's parent
 
+//In short, lexical space of a context --> lexical environment of local + lexical env referenced to it's parent
+
 //Lexical means --> In sequence or hierarchy
+
+//We can say that lexical parent of c is a, a has lexical parent global, and global has lexical parent pointing to null
 
 function a(){
     c()
