8de12248bf8366f33b6e0587e46be346eeeef963
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-10-23 17:55:53 +0530

    mini project

--- Fetch-Data-From-API.js
+++ Fetch-Data-From-API.js
@@ -1,14 +1,14 @@
 //You need to create a html file to navigate changes on browser
 ///Simply include this file inside <script> tag
 
-// Fetch users from the JSONPlaceholder API
+// Fetching users(name) from the jsonplaceholder API
 async function fetchUsers() {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const users = await response.json();
     return users;
 }
 
-// Display users on the page
+// Displaying users on the page
 function displayUsers(users) {
     const outputDiv = document.getElementById('output');
     outputDiv.innerHTML = "<h2>Users List:</h2>";
