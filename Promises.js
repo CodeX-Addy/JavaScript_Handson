f1192aa4057ba96af63ac29f7661aba64e6e6408
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-10-23 17:40:17 +0530

    .

--- Promises.js
+++ Promises.js
@@ -13,3 +13,31 @@ function greetlater(){
 }
 
 greetlater();
+
+//Now what is a promise?
+/*
+A promise in JavaScript is an object that represents the eventual completion (or failure) of an asynchronous operation. It allows you to handle asynchronous tasks more easily.
+
+A promise has three states:
+
+Pending: The initial state, when the operation is not yet completed.
+Resolved (Fulfilled): The operation completed successfully.
+Rejected: The operation failed
+*/
+
+const myPromise = new Promise((resolve, reject)=>{
+    let success = false;
+    if(success){
+        resolve("Message successfully sent..");
+    }
+    else{
+        reject("Message destroyed..");
+    }
+})
+
+//Calling promise here
+myPromise.then((message)=>{
+    console.log(message);
+}).catch((error) => {
+    console.log(error);
+})
