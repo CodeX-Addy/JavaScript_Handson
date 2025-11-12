fe04c6e63f47476e6a4a0fb4ece8724eccfa4818
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-11-12 22:07:41 +0530

    add: server

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res)=>{
-    res.send('Hey there!');
+    res.send('Hey there! Welcome to the Express.js server.');
 });
 
 app.listen(port, ()=>{
