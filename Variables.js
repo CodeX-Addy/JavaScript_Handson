ef5713afe251fd4ea4cf11067b19f941ea0d97b6
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-12-31 23:13:01 +0530

    add: variables

--- Variables.js
+++ Variables.js
@@ -1,4 +1,5 @@
 // Pointer 1: Dynamically typed
+
 let a = 8;
 // console.log(a); -> 8
 a = "Adi";
@@ -6,6 +7,7 @@ a = "Adi";
 
 // Pointer 2: var is globally, while let is locally scoped
 // Using let is a good practice
+
 var b = 10;
 var b = 20; //var can be redeclared while let don't
 // console.log(b); -> 20
@@ -22,6 +24,7 @@ console.log(a); //Adi
 console.log(b); //40
 
 //Pointer 3: const
+
 const x = "Esoteric";
 //x = "Eso"; //Error: Assignment to constant variable.
 console.log(x); //Esoteric
