0da0049d4a4c2bb4446763fa360a9176643eba45
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-01-25 20:52:38 +0530

    add: data types

--- Primitive-Objects.js
+++ Primitive-Objects.js
@@ -8,11 +8,11 @@ B : BigInt
 U : undefined */
 
 let a = null;
-let b = 527; //It could be floating point as well
+let b = 527; //It could be floating point as well, having range is -(2^53 - 1) to (2^53 - 1)
 let c = "Eso";
 let d = Symbol("A symbol type");
 let e = true;
-let f = BigInt("528");
+let f = BigInt("528"); // If we need larger numbers than Number's range
 let g;
 console.log(a,b,c,d,e,f,g);
 console.log(typeof a,typeof b,typeof c,typeof d,typeof e,typeof f, typeof g);
