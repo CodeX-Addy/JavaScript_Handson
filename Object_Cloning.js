1faf93c3de6144be1e0e730022c28696e5391203
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-02-07 23:44:51 +0530

    add: objects cloning

--- Object_Cloning.js
+++ Object_Cloning.js
@@ -20,3 +20,21 @@ let obj2 = Object.assign({}, obj1);
 
 console.log(obj1);
 console.log(obj2);
+
+// ********************************************************
+// USING ITERATION -> COMPLEX FOR NESTED OBJECTS
+// ********************************************************
+
+let obj1 = {age:23, name:"aditya"};
+
+let obj2 = {};
+
+for(i in obj1){
+    let key = i;
+    let value = obj1[key];
+    
+    obj2[key] = value;
+}
+
+console.log(obj1);
+console.log(obj2);
